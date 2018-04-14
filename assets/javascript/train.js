e69b119f632a89dbd38ced95f2352af954fb0e8c
kyletobin <35541078+kyletobin@users.noreply.github.com>
2018-04-13 23:06:33 -0500

    got keep it validated

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -21,3 +21,29 @@ database.ref().on("child_added", function (childSnap) {
 
   $("#trainTable > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + next + "</td><td>" + min + "</td></tr>");
 });
+
+database.ref().on("value", function (snapshot) { });
+
+$("#addTrainBtn").on("click", function () {
+
+  var trainName = $("#trainNameInput").val().trim();
+  var destination = $("#destinationInput").val().trim();
+  var firstTrain = $("#firstInput").val().trim();
+  var frequency = $("#frequencyInput").val().trim();
+
+  if (trainName == "") {
+    alert('Enter a train name.');
+    return false;
+  }
+  if (destination == "") {
+    alert('Enter a destination.');
+    return false;
+  }
+  if (firstTrain == "") {
+    alert('Enter a first train time.');
+    return false;
+  }
+  if (frequency == "") {
+    alert('Enter a frequency');
+    return false;
+  }
