83ca9d5005a900d81f23132b9bd3291381e96ce0
kyletobin <35541078+kyletobin@users.noreply.github.com>
2018-04-13 19:48:40 -0500

    keeping it basic

---
+++ assets/javascript/train.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyDF7LCvLnZvt1iUoIdmwiukiEOABAIJ8Z4",
+    authDomain: "traintime-c0b6b.firebaseapp.com",
+    databaseURL: "https://traintime-c0b6b.firebaseio.com",
+    projectId: "traintime-c0b6b",
+    storageBucket: "traintime-c0b6b.appspot.com",
+    messagingSenderId: "872081677737"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
