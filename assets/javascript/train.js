355528b4ea9efc9f8251626b9526d46f73d87db0
kyletobin <35541078+kyletobin@users.noreply.github.com>
2018-04-13 22:53:18 -0500

    Table Time!

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,11 +1,23 @@
 var config = {
-    apiKey: "AIzaSyDF7LCvLnZvt1iUoIdmwiukiEOABAIJ8Z4",
-    authDomain: "traintime-c0b6b.firebaseapp.com",
-    databaseURL: "https://traintime-c0b6b.firebaseio.com",
-    projectId: "traintime-c0b6b",
-    storageBucket: "traintime-c0b6b.appspot.com",
-    messagingSenderId: "872081677737"
+  apiKey: "AIzaSyDF7LCvLnZvt1iUoIdmwiukiEOABAIJ8Z4",
+  authDomain: "traintime-c0b6b.firebaseapp.com",
+  databaseURL: "https://traintime-c0b6b.firebaseio.com",
+  projectId: "traintime-c0b6b",
+  storageBucket: "traintime-c0b6b.appspot.com",
+  messagingSenderId: "872081677737"
 };
 firebase.initializeApp(config);
 
 var database = firebase.database();
+var currentTime = moment();
+
+database.ref().on("child_added", function (childSnap) {
+  var name = childSnap.val().name;
+  var destination = childSnap.val().destination;
+  var firstTrain = childSnap.val().firstTrain;
+  var frequency = childSnap.val().frequency;
+  var min = childSnap.val().min;
+  var next = childSnap.val().next;
+
+  $("#trainTable > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + next + "</td><td>" + min + "</td></tr>");
+});
